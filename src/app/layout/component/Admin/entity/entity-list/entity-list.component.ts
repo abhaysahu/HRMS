fa0e0b92cec50d6dce4aba4af94184e77a9f63ab
abhaysahu <42239059+abhaysahu@users.noreply.github.com>
2020-05-17 13:21:13 +0530

    show the entity table

--- src/app/layout/component/Admin/entity/entity-list/entity-list.component.ts
+++ src/app/layout/component/Admin/entity/entity-list/entity-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Entity } from '../model/entity';
+import { EntityService } from '../service/entity.service';
 
 @Component({
   selector: 'app-entity-list',
@@ -7,7 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EntityListComponent implements OnInit {
 
-  constructor() { }
+  entity: Entity[]=[];
+
+  constructor(private entityService: EntityService) { 
+
+    this.entityService.getEntityData().subscribe(resp =>{
+      console.log(resp)
+      this.entity = resp.Data
+      console.log(this.entity)
+    })
+  }
 
   ngOnInit() {
   }
