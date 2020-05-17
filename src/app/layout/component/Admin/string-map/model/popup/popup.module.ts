8737d8df9c2a9b0735d72ff59faba3b32b40fb79
aastha26 <aasthajain906@gmail.com>
2020-05-17 14:03:05 +0530

    add new in popup component

---
+++ src/app/layout/component/Admin/string-map/model/popup/popup.module.ts
@@ -0,0 +1,27 @@
+export class Popup { 
+
+  id:number;
+  projectid: number;
+  date: string;
+  monefforts:number;
+  mondesc:string
+  tusefforts:number;
+  tusdesc: string;
+  wedefforts:number;
+  weddesc:string;
+  thuefforts:number;
+  thudesc:string;
+  friefforts:number;
+  fridesc:string;
+  satefforts:number;
+  satdesc:string;
+  sunefforts:number;
+  sundesc:string;
+  status:number;
+
+
+
+  
+}
+
+
--- src/app/layout/component/Admin/string-map/popup/popup.component.ts
+++ src/app/layout/component/Admin/string-map/popup/popup.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { Popup } from '../model/popup/popup.module';
+
 
 
 @Component({
@@ -8,6 +10,9 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
   styleUrls: ['./popup.component.css']
 })
 export class PopupComponent implements OnInit {
+  Popup: any=[]
+
+  formData: Popup;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data,
@@ -21,6 +26,32 @@ export class PopupComponent implements OnInit {
   {
     this.dialogRef.close();
   }
+  AddProject(){
 
+    this.formData = {
+        
+      id:3,
+      projectid: 0,
+      date: '',
+      monefforts: 0,
+      mondesc:'',
+      tusefforts: 0,
+      tusdesc: '',
+      wedefforts: 0,
+      weddesc: '',
+      thuefforts: 0,
+      thudesc: '',
+      friefforts: 0,
+      fridesc: '',
+      satefforts: 0,
+      satdesc: '',
+      sunefforts: 0,
+      sundesc: '',
+      status: 0
+    }
+
+    /*this.timesheetService.timesheet.push(this.formData);
+    console.log(this.timesheetService.timesheet)*/
+  }
 
 }
