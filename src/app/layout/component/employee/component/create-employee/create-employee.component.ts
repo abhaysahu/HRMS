75215c9971aebd5310a27b6f0a1861be826a672b
abhaysahu <42239059+abhaysahu@users.noreply.github.com>
2020-05-25 11:38:51 +0530

    change in employee

--- src/app/layout/component/employee/component/create-employee/create-employee.component.ts
+++ src/app/layout/component/employee/component/create-employee/create-employee.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../../models/employee';
 import { Router } from '@angular/router';
+import { EmployeeService } from '../../services/employee.service';
+import { AppResponse } from 'src/app/models/appResponse';
 
 @Component({
   selector: 'app-create-employee',
@@ -11,11 +13,242 @@ export class CreateEmployeeComponent implements OnInit {
 
   Employee: any[]=[];
 
+  dropdownListOfDivision: any[]=[];
+  dropdownListOfDepartment: any[]=[];
+  dropdownListOfDesignation: any[]=[];
+  dropdownListOfMaritalStatus: any[]=[];
+  dropdownListOfGender: any[]=[];
+  
 
-  constructor(private router: Router) { }
+  dangerStatus=false;
+  successStatus=false;
+  message="";
+
+
+  constructor(private router: Router, private employeeService: EmployeeService) {
+
+
+    this.employeeService.listOfDivision().subscribe(resp =>{
+
+      console.log(resp)
+   
+      if(resp.Success)
+      {
+        this.dropdownListOfDivision = resp.Data
+      }
+      else
+      {
+        this.dangerStatus=true;
+        this.successStatus=false;
+        this.message=resp.ErrorMessage;
+        this.message=resp.Message;
+      }
+      
+    }
+    ,   (error: AppResponse) => {
+      if(error.status === 400)
+      {
+        this.dangerStatus=true;
+        this.successStatus=false;
+        this.message = error.message
+       
+      }
+      else if(error.status === 401)
+      {
+        this.dangerStatus=true;
+        this.successStatus=false;
+        this.message = "Authorization has been denied for this request And You have to Login again."
+        setTimeout(()=>
+          {    
+            this.router.navigate(['/login']);
+          }, 3000);
+      }       
+      else
+      {
+        this.dangerStatus=true;
+        this.successStatus=false;
+        this.message = error.message;
+      }
+    }
+  )
+
+  this.employeeService.listOfDepartment().subscribe(resp =>{
+   
+    if(resp.Success)
+    {
+      this.dropdownListOfDepartment = resp.Data
+    }
+    else
+    {
+      this.dangerStatus=true;
+      this.successStatus=false;
+      this.message=resp.ErrorMessage;
+      this.message=resp.Message;
+    }
+    
+  }
+  ,   (error: AppResponse) => {
+    if(error.status === 400)
+    {
+      this.dangerStatus=true;
+      this.successStatus=false;
+      this.message = error.message
+     
+    }
+    else if(error.status === 401)
+    {
+      this.dangerStatus=true;
+      this.successStatus=false;
+      this.message = "Authorization has been denied for this request And You have to Login again."
+      setTimeout(()=>
+          {    
+            this.router.navigate(['/login']);
+          }, 3000);
+    }       
+    else
+    {
+      this.dangerStatus=true;
+      this.successStatus=false;
+      this.message = error.message;
+    }
+  }
+)
+
+
+
+this.employeeService.listOfDesignation().subscribe(resp =>{
+   
+  if(resp.Success)
+  {
+    this.dropdownListOfDesignation = resp.Data
+  }
+  else
+  {
+    this.dangerStatus=true;
+    this.successStatus=false;
+    this.message=resp.ErrorMessage;
+    this.message=resp.Message;
+  }
+  
+}
+,   (error: AppResponse) => {
+  if(error.status === 400)
+  {
+    this.dangerStatus=true;
+    this.successStatus=false;
+    this.message = error.message
+   
+  }
+  else if(error.status === 401)
+  {
+    this.dangerStatus=true;
+    this.successStatus=false;
+    this.message = "Authorization has been denied for this request And You have to Login again."
+    setTimeout(()=>
+          {    
+            this.router.navigate(['/login']);
+          }, 3000);
+  }       
+  else
+  {
+    this.dangerStatus=true;
+    this.successStatus=false;
+    this.message = error.message;
+  }
+}
+)
+
+this.employeeService.listOfMaritalStatus().subscribe(resp =>{
+   
+  if(resp.Success)
+  {
+    this.dropdownListOfMaritalStatus = resp.Data
+  }
+  else
+  {
+    this.dangerStatus=true;
+    this.successStatus=false;
+    this.message=resp.ErrorMessage;
+    this.message=resp.Message;
+  }
+  
+}
+,   (error: AppResponse) => {
+  if(error.status === 400)
+  {
+    this.dangerStatus=true;
+    this.successStatus=false;
+    this.message = error.message
+   
+  }
+  else if(error.status === 401)
+  {
+    this.dangerStatus=true;
+    this.successStatus=false;
+    this.message = "Authorization has been denied for this request And You have to Login again."
+    setTimeout(()=>
+          {    
+            this.router.navigate(['/login']);
+          }, 3000);
+  }       
+  else
+  {
+    this.dangerStatus=true;
+    this.successStatus=false;
+    this.message = error.message;
+  }
+}
+)
+
+
+this.employeeService.listOfGender().subscribe(resp =>{
+   
+  if(resp.Success)
+  {
+    this.dropdownListOfGender = resp.Data
+  }
+  else
+  {
+    this.dangerStatus=true;
+    this.successStatus=false;
+    this.message=resp.ErrorMessage;
+    this.message=resp.Message;
+  }
+  
+}
+,   (error: AppResponse) => {
+  if(error.status === 400)
+  {
+    this.dangerStatus=true;
+    this.successStatus=false;
+    this.message = error.message
+   
+  }
+  else if(error.status === 401)
+  {
+    this.dangerStatus=true;
+    this.successStatus=false;
+    this.message = "Authorization has been denied for this request And You have to Login again."
+    setTimeout(()=>
+          {    
+            this.router.navigate(['/login']);
+          }, 3000);
+  }       
+  else
+  {
+    this.dangerStatus=true;
+    this.successStatus=false;
+    this.message = error.message;
+  }
+}
+)
+
+}
+
 
   ngOnInit() {
   }
+
 
   Basic(employee) {
     console.log(employee)
--- src/app/layout/component/employee/services/employee.service.ts
+++ src/app/layout/component/employee/services/employee.service.ts
@@ -1,15 +1,85 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { DropDown } from '../models/dropDown';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
   getemployee()
   {
-    return this.http.get('assets/emplyee_details.json');
+    return this.httpClient.get('assets/emplyee_details.json');
   }
+
+
+  listOfDivision(): Observable<DropDown>
+  {
+    const token = sessionStorage.getItem('token')
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'authorization': `bearer ${token}`
+    })
+
+    return this.httpClient.get<DropDown>(environment.webapiUrl+'api/get/entity', { headers: headers })
+  }
+  
+
+  listOfDepartment(): Observable<DropDown>
+  {
+    const token = sessionStorage.getItem('token')
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'authorization': `bearer ${token}`
+    })
+
+    return this.httpClient.get<DropDown>(environment.webapiUrl+'api/get/entity', { headers: headers })
+  }
+
+
+  listOfDesignation(): Observable<DropDown>
+  {
+    const token = sessionStorage.getItem('token')
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'authorization': `bearer ${token}`
+    })
+
+    return this.httpClient.get<DropDown>(environment.webapiUrl+'api/get/entity', { headers: headers })
+  }
+  
+
+  listOfMaritalStatus(): Observable<DropDown>
+  {
+    const token = sessionStorage.getItem('token')
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'authorization': `bearer ${token}`
+    })
+
+    return this.httpClient.get<DropDown>(environment.webapiUrl+'api/get/entity', { headers: headers })
+  }
+
+
+  listOfGender(): Observable<DropDown>
+  {
+    const token = sessionStorage.getItem('token')
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'authorization': `bearer ${token}`
+    })
+
+    return this.httpClient.get<DropDown>(environment.webapiUrl+'api/get/entity', { headers: headers })
+  }
+
+
 }
