b245d69a4e67ac08648d99b94193a9d130933961
aastha26 <aasthajain906@gmail.com>
2020-05-26 22:25:38 +0530

    sorting in string-map

--- src/app/layout/component/Admin/string-map/component/string-map-list/string-map-list.component.ts
+++ src/app/layout/component/Admin/string-map/component/string-map-list/string-map-list.component.ts
@@ -16,6 +16,29 @@ export class StringMapListComponent implements OnInit {
 
   stringMapList: StringMapList[]=[];
   dropdownList: DropDownList[]=[];
+  ascNumberSort = true;
+  indexs:any[]=[];
+  stringMap:any[]=[];
+  
+  sortIcon1="fa fa-sort"
+  sortIcon2="fa fa-sort"
+  sortIcon3="fa fa-sort"
+  sortIcon4="fa fa-sort"
+  sortIcon5="fa fa-sort"
+  sortIcon6="fa fa-sort"
+  sortIcon7="fa fa-sort"
+  sortIcon8="fa fa-sort"
+
+
+ 
+
+
+
+  email="";
+
+  aa:boolean=false;
+  search="";
+
 
   successStatus=false;
   dangerStatus=false;
@@ -25,7 +48,15 @@ export class StringMapListComponent implements OnInit {
 
   constructor(private dialog: MatDialog,
               private stringmapService: StringMapService,
-              ){                
+              ){  
+                this.stringMap=[{
+                  EntityName: null,
+                  AttributeName: null,
+                  CountStringMap: null,
+          
+                }]
+            
+
 
     this.stringmapService.getdropdownData().subscribe(resp =>{
    
@@ -75,6 +106,75 @@ export class StringMapListComponent implements OnInit {
 
   ngOnInit() {
   }
+  sortFilter(value)
+  {
+    this.disable();
+
+    if(value == 1)
+    {
+      this.ascNumberSort = !this.ascNumberSort;
+      if(this.ascNumberSort) 
+      {
+        this.sortIcon1="fa fa-sort-desc"
+        this.stringMap=this.stringMap.sort((a,b)=>a.Name.localeCompare(b.Name)); // For ascending sort
+      } 
+      else 
+      {
+        this.sortIcon1="fa fa-sort-asc"
+        this.stringMap=this.stringMap.sort((a,b)=>b.Name.localeCompare(a.Name)); // For descending sort
+      }
+    }
+
+    else if(value == 2)
+    {
+      this.ascNumberSort = !this.ascNumberSort;
+      if(this.ascNumberSort) 
+      {
+        this.sortIcon2="fa fa-sort-desc"
+        this.stringMap=this.stringMap.sort((a,b)=>a.LogicalName.localeCompare(b.LogicalName)); // For ascending sort
+      } 
+      else 
+      {
+        this.sortIcon2="fa fa-sort-asc"
+        this.stringMap=this.stringMap.sort((a,b)=>b.LogicalName.localeCompare(a.LogicalName)); // For descending sort
+      }
+    }
+
+    else if(value == 3)
+    {
+      this.ascNumberSort = !this.ascNumberSort;
+      if(this.ascNumberSort) 
+      {
+        this.sortIcon3="fa fa-sort-desc"
+        this.stringMap=this.stringMap.sort((a,b)=>a.SchemaName.localeCompare(b.SchemaName)); // For ascending sort
+      } 
+      else 
+      {
+        this.sortIcon3="fa fa-sort-asc"
+        this.stringMap=this.stringMap.sort((a,b)=>b.SchemaName.localeCompare(a.SchemaName)); // For descending sort
+      }
+    }
+
+    
+
+   
+
+   
+   
+
+    
+  }
+  disable()
+  {
+    this.sortIcon1="fa fa-sort"
+    this.sortIcon2="fa fa-sort"
+    this.sortIcon3="fa fa-sort"
+  }
+
+  setIndex(ii){
+    this.aa=ii;
+  }
+
 
   AddOrEditOrderItem(stringMapList)
   { 
