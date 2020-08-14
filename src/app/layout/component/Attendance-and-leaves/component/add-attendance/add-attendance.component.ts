0cc7b7445343d60e1c0584aac49f0d20f65be9f9
abhaysahu <42239059+abhaysahu@users.noreply.github.com>
2020-08-14 11:51:13 +0530

    add attendance

--- src/app/layout/component/Attendance-and-leaves/component/add-attendance/add-attendance.component.ts
+++ src/app/layout/component/Attendance-and-leaves/component/add-attendance/add-attendance.component.ts
@@ -23,9 +23,6 @@ export class AddAttendanceComponent implements OnInit {
   showStatus=false;
 
 
-
-
-
   constructor(
     private attendanceService: AttendanceService,
     private customToastrService: CustomToastrService,
@@ -65,57 +62,7 @@ export class AddAttendanceComponent implements OnInit {
   
       }
     );
-  
-
-      this.attendanceService.getUser().subscribe(resp => {
-
-        if(resp.Success)
-        {
-          this.recordData = resp.Data
-         
-          for (let i=0; i<this.recordData.length; i++)
-          {
-            let date={
-              EmployeeId: this.recordData[i].Id,
-              Name: this.recordData[i].FullName,
-              InTime: "00:00",
-              OutTime: "00:00",
-              CreatedBy: JSON.parse(sessionStorage.getItem('user')).Id,
-              Status: 1,
-              Action: 1
-            }
-            this.Addattendance.push(date)
-          }
-  
-          console.log(this.Addattendance)
-
-          var b2 = [
-            {EmployeeId: "0",Name: "a",InTime: "08:25",OutTime: "19:25",CreatedBy: JSON.parse(sessionStorage.getItem('user')).Id,Status: 1,Action: 2},
-            {EmployeeId: "1",Name: "b",InTime: "09:25",OutTime: "20:25",CreatedBy: JSON.parse(sessionStorage.getItem('user')).Id,Status: 2,Action: 2},
-            {EmployeeId: "5",Name: "f",InTime: "10:25",OutTime: "21:25",CreatedBy: JSON.parse(sessionStorage.getItem('user')).Id,Status: 3,Action: 2},
-          ];
-
-          for(let i=0;i<b2.length;i++)
-          {
-            this.Addattendance.push(b2[i]);
-          }
-  
-        }
-        else
-        {
-            this.message=resp.ErrorMessage;
-            this.message=resp.Message;
-            this.customToastrService.GetErrorToastr(this.message,"Add Attendance", 5000)
-        }
-  
-      },   (error: AppResponse) => {
-        this.errorHandlingService.errorStatus(error,"Add Attendance")
-  
-  }
-  )
-
-    }
-
+}
  ngOnInit() {
   }
 
@@ -128,7 +75,8 @@ export class AddAttendanceComponent implements OnInit {
      
       if (resp.Success) {
         this.GetAttendance = resp.Data;
-        this.AddAttendanceOfEmployee(this.GetAttendance)
+        this.Addattendance = resp.Data
+       
       } else {
         
         this.message = resp.ErrorMessage;
@@ -145,50 +93,6 @@ export class AddAttendanceComponent implements OnInit {
     }
   );
 
-
-  }
-
-  AddAttendanceOfEmployee(Attendance)
-  {
-    this.showStatus=true
-    console.log(this.Addattendance)
-
-  }
-
-
-  Save()
-  {
-    console.log(this.Addattendance)
-
-    var b1 = [
-      {EmployeeId: "0",Name: "a",InTime: "00:00",OutTime: "00:00",CreatedBy: JSON.parse(sessionStorage.getItem('user')).Id,Status: 1,Action: 1},
-      {EmployeeId: "1",Name: "b",InTime: "00:00",OutTime: "00:00",CreatedBy: JSON.parse(sessionStorage.getItem('user')).Id,Status: 1,Action: 1},
-      {EmployeeId: "2",Name: "c",InTime: "00:00",OutTime: "00:00",CreatedBy: JSON.parse(sessionStorage.getItem('user')).Id,Status: 1,Action: 1},
-      {EmployeeId: "3",Name: "d",InTime: "00:00",OutTime: "00:00",CreatedBy: JSON.parse(sessionStorage.getItem('user')).Id,Status: 1,Action: 1},
-      {EmployeeId: "4",Name: "e",InTime: "00:00",OutTime: "00:00",CreatedBy: JSON.parse(sessionStorage.getItem('user')).Id,Status: 1,Action: 1},
-      {EmployeeId: "5",Name: "f",InTime: "00:00",OutTime: "00:00",CreatedBy: JSON.parse(sessionStorage.getItem('user')).Id,Status: 1,Action: 1},
-      
-    ]; 
-    
-    var b2 = [
-      {EmployeeId: "0",Name: "a",InTime: "08:25",OutTime: "19:25",CreatedBy: JSON.parse(sessionStorage.getItem('user')).Id,Status: 1,Action: 2},
-      {EmployeeId: "1",Name: "b",InTime: "09:25",OutTime: "20:25",CreatedBy: JSON.parse(sessionStorage.getItem('user')).Id,Status: 2,Action: 2},
-      {EmployeeId: "5",Name: "f",InTime: "10:25",OutTime: "21:25",CreatedBy: JSON.parse(sessionStorage.getItem('user')).Id,Status: 3,Action: 2},
-      
-    ];
-
-    var res = b1.filter(item1 => 
-      !b2.some(item2 => (item2.EmployeeId === item1.EmployeeId)))
-      console.log(res);
-
-      for(let i=0;i<res.length;i++)
-      {
-        b2.push(res[i]);
-      }
-
-      this.Addattendance = b2;
-      this.showStatus=true
-      console.log(this.Addattendance);
 
   }
 
@@ -211,7 +115,6 @@ export class AddAttendanceComponent implements OnInit {
 
   SaveAttendance(employeeId)
   {
-
 
     this.FormData[0].EmployeeId=employeeId;
     this.FormData[0].CreatedBy=JSON.parse(sessionStorage.getItem('user')).Id;
@@ -236,29 +139,19 @@ export class AddAttendanceComponent implements OnInit {
           CreatedBy: "",
           Status: ""
         }]
-
         this.getUserForAttendanceByDate(this.DateOfAttendance)
-        
       }
-
       else
       {
         this.message=resp.Message;
         this.customToastrService.GetErrorToastr(this.message, "Attendance Save Status", 5000)
-
       }
-      
     }
     ,   (error: AppResponse) => {
       this.errorHandlingService.errorStatus(error,"Attendance Save Status")
-
 }
 )
 
-
-
-
   }
-
 
 }
