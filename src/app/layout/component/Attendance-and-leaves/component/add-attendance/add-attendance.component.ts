83d2f172051db5596b5d3444718606712fa602a6
aastha26 <aasthajain906@gmail.com>
2020-08-25 20:05:56 +0530

    changes in My attendance

--- src/app/layout/component/Attendance-and-leaves/component/add-attendance/add-attendance.component.ts
+++ src/app/layout/component/Attendance-and-leaves/component/add-attendance/add-attendance.component.ts
@@ -10,8 +10,10 @@ import { ErrorHandlingService } from 'src/app/service/error-handling.service';
   styleUrls: ['./add-attendance.component.css']
 })
 export class AddAttendanceComponent implements OnInit {
+    persons: any[]=[];
+
   Addattendance: any[] = [];
-  
+
   respnseOfAttendace:any=[];
 
 
@@ -38,15 +40,15 @@ export class AddAttendanceComponent implements OnInit {
       // let d = new Date();
       let d = new Date();
       let calculatedDate =new Date(d.setDate(d.getDate()));
-  
+
       let date12 =(`${calculatedDate.getMonth()+1}-${calculatedDate.getDate()}-${calculatedDate.getFullYear()}`);
-  
+
       // console.log(date12)
-  
+
       this.ProductForm = {
         Date: date12,
       }
-  
+
       console.log(this.ProductForm)
 
       this.getUserForAttendanceByDate(this.ProductForm.Date)
@@ -65,23 +67,23 @@ export class AddAttendanceComponent implements OnInit {
 
       this.attendanceService.AttendanceStatus().subscribe(resp => {
         console.log(resp);
-       
+
         if (resp.Success) {
           this.dropdownList = resp.Data;
           console.log(this.dropdownList)
         } else {
-          
+
           this.message = resp.ErrorMessage;
           this.message = resp.Message;
           this.customToastrService.GetErrorToastr(this.message, "Attendance Status", 5000)
-  
+
         }
-        
+
       }
       ,   (error: AppResponse) => {
-  
+
         this.errorHandlingService.errorStatus(error,"Attendance Status")
-  
+
       }
     );
 }
@@ -95,12 +97,12 @@ export class AddAttendanceComponent implements OnInit {
 
     this.attendanceService.getAttendanceByDate(date).subscribe(resp => {
       console.log(resp)
-     
+
       if (resp.Success) {
         this.Addattendance = resp.Data;
-        
+
         for(let i=0;i<resp.Data.length;i++)
-        { 
+        {
           if(this.Addattendance[i].Status!=null)
           {
             this.Addattendance[i].Status=this.Addattendance[i].Status.Value
@@ -126,18 +128,18 @@ export class AddAttendanceComponent implements OnInit {
 
         this.showStatus=true;
 
-       
+
       } else {
-        
+
         this.message = resp.ErrorMessage;
         this.message = resp.Message;
         this.customToastrService.GetErrorToastr(this.message, "Attendance Status", 5000)
       }
     }
     ,   (error: AppResponse) => {
-  
+
       this.errorHandlingService.errorStatus(error,"Attendance Status")
-  
+
     }
   );
 
@@ -147,9 +149,9 @@ export class AddAttendanceComponent implements OnInit {
 
   Statuschange(status,i)
   {
-    
+
     this.Addattendance[i].Status=status
-    
+
     this.FormData[0].Status = status
     if(status==3 || status==4)
     {
@@ -162,7 +164,7 @@ export class AddAttendanceComponent implements OnInit {
       this.Addattendance[i].OutTime=null;
     }
 
-    
+
   }
 
   InTimechange(inTime,i)
@@ -195,16 +197,16 @@ export class AddAttendanceComponent implements OnInit {
     {
       this.FormData[0].OutTime=this.Addattendance[i].OutTime
     }
-    
+
     console.log(this.FormData)
 
 
     this.attendanceService.attendanceDataSave(this.FormData[0]).subscribe(resp => {
-     
+
       if(resp.Success)
       {
         this.message="Data is Added successfully"
-        
+
         this.customToastrService.GetSuccessToastr(this.message, "Attendance Save Status", 5000)
 
         this.FormData=[{
@@ -248,16 +250,16 @@ export class AddAttendanceComponent implements OnInit {
     {
       this.FormData[0].OutTime=this.Addattendance[i].OutTime
     }
-    
+
     console.log(this.FormData)
 
     this.attendanceService.attendanceDataUpdate(this.FormData[0]).subscribe(resp => {
-     
+
       if(resp.Success)
       {
         this.message="Data is Updated successfully"
         console.log(resp)
-        
+
         this.customToastrService.GetSuccessToastr(this.message, "Attendance Update Status", 5000)
 
         this.FormData=[{
