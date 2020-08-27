017d7e25383ca94501f96977f5400cea549422b5
abhaysahu <42239059+abhaysahu@users.noreply.github.com>
2020-08-27 21:03:29 +0530

    adding service in request

--- src/app/layout/component/Service-request/component/add-sr/add-sr.component.ts
+++ src/app/layout/component/Service-request/component/add-sr/add-sr.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AddSr } from '../../models/add_sr';
+import { ServiceRequestsService } from '../../service/service-requests.service';
+import { AppResponse } from 'src/app/models/appResponse';
+import { ErrorHandlingService } from 'src/app/service/error-handling.service';
+import { CustomToastrService } from 'src/app/service/customToastr.service';
 
 @Component({
   selector: 'app-add-sr',
@@ -7,15 +11,107 @@ import { AddSr } from '../../models/add_sr';
   styleUrls: ['./add-sr.component.css']
 })
 export class AddSRComponent implements OnInit {
-  Sr ={} as AddSr
+  Sr ={} as AddSr;
+  dropDownListOfFunction:any[]=[]
+  dropDownListOfSubFunction:any[]=[]
+  dropDownListOfPriority:any[]=[]
 
-  
-  constructor() { }
+  message;
+
+  constructor(
+    private requestsService: ServiceRequestsService,
+    private customToastrService: CustomToastrService,
+    private errorHandlingService: ErrorHandlingService
+    ) { 
+
+
+    this.requestsService.listOfFunction().subscribe(resp => {
+      console.log(resp);
+
+      if (resp.Success) {
+        this.dropDownListOfFunction = resp.Data;
+      } else {
+
+        this.message = resp.ErrorMessage;
+        this.message = resp.Message;
+        this.customToastrService.GetErrorToastr(this.message, "Add Service Request Status", 5000)
+
+      }
+    }
+    ,   (error: AppResponse) => {
+      this.errorHandlingService.errorStatus(error,"Add Service Request Status")
+    }
+  );
+
+
+  this.requestsService.listOfSubFunction().subscribe(resp => {
+    console.log(resp);
+
+    if (resp.Success) {
+      this.dropDownListOfSubFunction = resp.Data;
+    } else {
+
+      this.message = resp.ErrorMessage;
+      this.message = resp.Message;
+      this.customToastrService.GetErrorToastr(this.message, "Add Service Request Status", 5000)
+
+    }
+  }
+  ,   (error: AppResponse) => {
+    this.errorHandlingService.errorStatus(error,"Add Service Request Status")
+  }
+);
+
+
+
+this.requestsService.listOfPriority().subscribe(resp => {
+  console.log(resp);
+
+  if (resp.Success) {
+    this.dropDownListOfPriority = resp.Data;
+  } else {
+
+    this.message = resp.ErrorMessage;
+    this.message = resp.Message;
+    this.customToastrService.GetErrorToastr(this.message, "Add Service Request Status", 5000)
+
+  }
+}
+,   (error: AppResponse) => {
+  this.errorHandlingService.errorStatus(error,"Add Service Request Status")
+}
+);
+
+
+  }
 
   ngOnInit() {
   }
   onSubmit(sr) {
-    console.log(sr)  
+
+    this.requestsService.serviceRequestDataSave(sr).subscribe(resp => {
+
+      if(resp.Success)
+      {
+        this.message="Service Request is Added successfully"
+
+        this.customToastrService.GetSuccessToastr(this.message, "Service Request Status", 5000)
+      }
+
+      else
+      {
+        this.message=resp.Message;
+        this.customToastrService.GetErrorToastr(this.message, "Service Request Status", 5000)
+
+      }
+
+    }
+    ,   (error: AppResponse) => {
+      this.errorHandlingService.errorStatus(error,"Service Request Status")
+
+}
+)
+
   }
 }
 
