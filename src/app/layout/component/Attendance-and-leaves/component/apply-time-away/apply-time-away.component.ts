23b290a34cb5a6900a46160b11ef3d2c84374678
aastha26 <aasthajain906@gmail.com>
2020-09-02 20:36:10 +0530

    resolving error in apply time away

--- src/app/layout/component/Attendance-and-leaves/component/apply-time-away/apply-time-away.component.ts
+++ src/app/layout/component/Attendance-and-leaves/component/apply-time-away/apply-time-away.component.ts
@@ -7,6 +7,19 @@ import { ApplyTimeAway } from '../../models/time_away';
   styleUrls: ['./apply-time-away.component.css']
 })
 export class ApplyTimeAwayComponent implements OnInit {
+  options = [
+    {
+        id: "Personal Leave",
+        value: '1'
+    },
+    {
+        id: "Sick Leave",
+        value: '2'
+    },
+    {
+        id: "Emergency Leave",
+        value: '3'
+    }];
 
   Attendance ={} as ApplyTimeAway;
 
